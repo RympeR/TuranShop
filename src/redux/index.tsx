a10667ebb2f09611fcbb8b7700936f01eda18f75
George <georg.rashkov@gmail.com>
2021-09-07 09:52:29 +0300

    small redux fix

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -7,15 +7,15 @@ const rootReducer  = () => combineReducers({
     product: productReducer,
     token: tokenReducer,
 });
-type rootReducer = typeof reducers;
+// type rootReducer = typeof reducers;
 
 type PropertiesType<T> = T extends { [key: string]: infer U } ? U : never;
 
 export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesType<T>>;
 
-export type RootState = ReturnType<rootReducer>;
+// export type RootState = ReturnType<rootReducer>;
 
-const store = createStore(reducers, compose(applyMiddleware(thunkMiddleware)));
+// const store = createStore(reducers, compose(applyMiddleware(thunkMiddleware)));
 
 const configureStore = () => {
     const middlewares = [thunk];
